e625cd2a7f92c87240f9d7900d825aa8de0cb4d9
Matt <Mattxiong_14@hotmail.com>
2021-09-24 09:35:59 -0500

    can see the list of dogs now

---
+++ Controllers/DogController.cs
@@ -0,0 +1,32 @@
+﻿using DogGo.Models;
+using DogGo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DogGo.Controllers
+{
+
+    public class DogController : Controller
+    {
+        private readonly IDogRepository _dogRepo;
+
+        // ASP.NET will give us an instance of our Walker Repository. This is called "Dependency Injection"
+        public DogController(IDogRepository dogRepository)
+        {
+            _dogRepo = dogRepository;
+        }
+
+        public ActionResult Index()
+        {
+            List<Dog> dogs = _dogRepo.GetAllDogs();
+
+            return View(dogs);
+        }
+    }
+}
--- Repositories/IDogRepository.cs
+++ Repositories/IDogRepository.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DogGo.Models;
+
 
 namespace DogGo.Repositories
 {
-    public class IDogRepository
+    public interface IDogRepository
     {
+        List<Dog> GetAllDogs();
     }
 }
